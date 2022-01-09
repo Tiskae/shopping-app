8659cb3d2a94de7972459f8ea6517c7c803faaf7
user <user@users-MacBook-Pro.local>
2022-01-09 18:44:22 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import productReducer from "./store/reducers/products";
+
+const rootReducer = combineReducers({
+  products: productReducer,
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/shop/ProductOverview.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+
+const ProductOverviewScreen = (props) => {
+  return <View style={styles.container}></View>;
+};
+
+export default ProductOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+  },
+});
