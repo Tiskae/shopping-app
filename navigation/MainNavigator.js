1b64dfa8f23886e34b8f1ca5971346405eaf81aa
user <user@users-MacBook-Pro.local>
2022-01-28 09:52:57 +0100

    Added authentication screen

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -20,93 +20,93 @@ const Drawer = createDrawerNavigator();
 const MainNavigator = () => {
   return (
     // <NavigationContainer>
-    <>
-      <StatusBar
-        backgroundColor={Platform.OS === "android" ? colors.primary : "white"}
-        translucent={false}
-        barStyle={Platform === "android" ? "dark-content" : "light-content"}
-      />
-      <Drawer.Navigator
-        screenOptions={{
-          gestureEnabled: true,
-          drawerType: "front",
-          headerTitleAlign: "center",
-          headerShown: false,
-          drawerActiveTintColor: colors.primary,
-          drawerContentStyle: {
-            padding: 0,
-            margin: 0,
-          },
+
+    <Drawer.Navigator
+      screenOptions={{
+        gestureEnabled: true,
+        drawerType: "front",
+        headerTitleAlign: "center",
+        headerShown: false,
+        drawerActiveTintColor: colors.primary,
+        drawerContentStyle: {
+          padding: 0,
+          margin: 0,
+        },
+      }}
+    >
+      <Drawer.Screen
+        name="ProductsNavigator"
+        component={ProductsNavigator}
+        options={{
+          title: "Products",
+          drawerIcon: (drawerConfig) => (
+            <Ionicons
+              name={Platform.OS === "android" ? "md-cart" : "ios-cart"}
+              size={23}
+              color={drawerConfig.color}
+            />
+          ),
         }}
-      >
-        <Drawer.Screen
-          name="ProductsNavigator"
-          component={ProductsNavigator}
-          options={{
-            title: "Products",
-            drawerIcon: (drawerConfig) => (
-              <Ionicons
-                name={Platform.OS === "android" ? "md-cart" : "ios-cart"}
-                size={23}
-                color={drawerConfig.color}
-              />
-            ),
-          }}
-        />
-        <Drawer.Screen
-          name="OrdersNavigator"
-          component={OrdersNavigator}
-          options={{
-            title: "Your Orders",
-            drawerIcon: (drawerConfig) => (
-              <Ionicons
-                name={Platform.OS === "android" ? "md-list" : "ios-list"}
-                size={23}
-                color={drawerConfig.color}
-              />
-            ),
-          }}
-        />
-        <Drawer.Screen
-          name="AdminNavigator"
-          component={AdminNavigator}
-          options={{
-            title: "Admin",
-            drawerIcon: (drawerConfig) => (
-              <Ionicons
-                name={Platform.OS === "android" ? "md-create" : "ios-create"}
-                size={23}
-                color={drawerConfig.color}
-              />
-            ),
-          }}
-        />
-      </Drawer.Navigator>
-    </>
+      />
+      <Drawer.Screen
+        name="OrdersNavigator"
+        component={OrdersNavigator}
+        options={{
+          title: "Your Orders",
+          drawerIcon: (drawerConfig) => (
+            <Ionicons
+              name={Platform.OS === "android" ? "md-list" : "ios-list"}
+              size={23}
+              color={drawerConfig.color}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="AdminNavigator"
+        component={AdminNavigator}
+        options={{
+          title: "Admin",
+          drawerIcon: (drawerConfig) => (
+            <Ionicons
+              name={Platform.OS === "android" ? "md-create" : "ios-create"}
+              size={23}
+              color={drawerConfig.color}
+            />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
     // </NavigationContainer>
   );
 };
 /////////////////////////////////////
 const FinalNavigator = () => (
   <NavigationContainer>
+    <StatusBar
+      backgroundColor={Platform.OS === "android" ? colors.primary : "white"}
+      translucent={false}
+      barStyle={Platform === "android" ? "dark-content" : "light-content"}
+    />
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="MainNavigator" component={MainNavigator} />
+      {/* <Stack.Screen name="MainNavigator" component={MainNavigator} /> */}
       <Stack.Screen
         name="AuthScreen"
         component={AuthScreen}
         options={{
-          headerShown: true,
           headerTitle: "Authenticate",
+          headerShown: true,
           headerStyle: {
             backgroundColor:
               Platform.OS === "android" ? colors.primary : "white",
           },
           headerTintColor: Platform.OS === "android" ? "white" : colors.primary,
           headerTitleStyle: { fontFamily: "open-sans-bold" },
-          headerTitleAlign: "center",
+          headerTitleAlign: "left",
         }}
       />
     </Stack.Navigator>
+    <Stack.Screen name="MainNavigator" component={MainNavigator} />
   </NavigationContainer>
 );
 
